92a0208c3b080b2deaddc92756d822cabc29d49c
Benning <60070109@kmitl.ac.th>
2020-02-19 22:13:28 +0700

    Modify cart admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,7 +6,18 @@ from core import models
 
 class OrderDetailInLine(admin.TabularInline):
     model = models.OrderDetail
-    extra = 1
+    extra = 0
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class CartDetailInLine(admin.TabularInline):
+    model = models.CartDetail
+    extra = 0
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
 
 class OrderAdmin(admin.ModelAdmin):
@@ -27,13 +38,24 @@ class OrderAdmin(admin.ModelAdmin):
     set_delivery_to_shipping.short_description = "Change selected waiting to shipping stage"
 
 
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'code', 'checkout_status']
+    list_display_links = ['user']
+    list_filter = ['checkout_status']
+    list_per_page = 10
+
+    search_fields = ['user__username']
+
+    inlines = [CartDetailInLine]
+
+
 admin.site.register(models.User)
 admin.site.register(models.Profile)
 admin.site.register(models.Address)
 admin.site.register(models.Category)
 admin.site.register(models.Product)
 admin.site.register(models.Code)
-admin.site.register(models.Cart)
+admin.site.register(models.Cart, CartAdmin)
 admin.site.register(models.CartDetail)
 admin.site.register(models.Order, OrderAdmin)
 admin.site.register(models.OrderDetail)
