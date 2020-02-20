220c50d49d74ccac4b1bd81c713e99a109212e30
Benning <60070109@kmitl.ac.th>
2020-02-20 15:34:06 +0700

    fix all admin panel

--- app/core/admin.py
+++ app/core/admin.py
@@ -12,8 +12,24 @@ class OrderDetailInLine(admin.TabularInline):
         return False
 
 
+class ProfileDetailInLine(admin.TabularInline):
+    model = models.Profile
+    extra = 0
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
 class CartDetailInLine(admin.TabularInline):
     model = models.CartDetail
+    extra = 0
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class AddressDetailInLine(admin.TabularInline):
+    model = models.Address
     extra = 0
 
     def has_change_permission(self, request, obj=None):
@@ -50,16 +66,69 @@ class CartAdmin(admin.ModelAdmin):
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'category', 'title', 'pic1', 'description', 'price', 'quantity']
+    list_display = ['id', 'title', 'category', 'description', 'price', 'quantity']
+    list_display_links = ['title']
+    list_editable = ['quantity']
+    list_filter = ['category']
+    list_per_page = 20
+
+    search_fields = ['title']
 
 
-admin.site.register(models.User)
-admin.site.register(models.Profile)
-admin.site.register(models.Address)
-admin.site.register(models.Category)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'first_name', 'last_name', 'is_active', 'is_staff']
+    list_display_links = ['id', 'username']
+    list_filter = ['is_staff', 'is_active']
+
+    inlines = [ProfileDetailInLine, AddressDetailInLine]
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'tel', 'dob', 'sex']
+    list_filter = ['sex']
+    list_display_links = ['user']
+
+    search_fields = ['user__username']
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'street', 'house_number', 'post_code', 'city']
+    list_display_links = ['id', 'user']
+    list_filter = ['city']
+
+    search_fields = ['user__username']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'type', 'color']
+    list_display_links = ['id', 'type']
+
+    search_fields = ['type']
+
+
+class CartDetailAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cart', 'product', 'quantity', 'price']
+    list_display_links = ['id', 'cart']
+
+    search_fields = ['cart__user__username']
+
+
+class CodeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'percent', 'start_date', 'end_date', 'quantity']
+    list_display_links = ['id', 'name']
+    list_editable = ['quantity']
+    list_filter = ['percent']
+
+    search_fields = ['name']
+
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Profile, ProfileAdmin)
+admin.site.register(models.Address, AddressAdmin)
+admin.site.register(models.Category, CategoryAdmin)
 admin.site.register(models.Product, ProductAdmin)
-admin.site.register(models.Code)
+admin.site.register(models.Code, CodeAdmin)
 admin.site.register(models.Cart, CartAdmin)
-admin.site.register(models.CartDetail)
+admin.site.register(models.CartDetail, CartDetailAdmin)
 admin.site.register(models.Order, OrderAdmin)
 admin.site.register(models.OrderDetail)
