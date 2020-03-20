90a9313a1def8236edcfe696404a194ba3cae0be
Benning <60070109@kmitl.ac.th>
2020-03-20 22:19:21 +0700

    production

--- app/manage.py
+++ app/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.development')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
